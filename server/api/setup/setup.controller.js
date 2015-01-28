8e076903a1ee0e9364d59036c20ff70c991ab15b
Ed Sykes <ed.sykes@gmail.com>
2015-01-28 07:50:27 +0000

    changed the setup to use the library rather than calling rest api directly

--- server/api/setup/setup.controller.js
+++ server/api/setup/setup.controller.js
@@ -7,18 +7,12 @@
 
 var _ = require('lodash');
 var request = require('request');
+var Lib1selfserver = require('lib1self-server');
 
 // Get list of things
 exports.index = function(req, res) {
   console.log(req.body.username);
     var username = req.body.username;
-    var callbackUrl = 'https://localhost:9001/api/sync?username='
-    					+ username
-    					+ '&lastSyncDate={{lastSyncDate}}'
-    					+ '&streamId={{streamId}}';
-
-    res.setHeader("Content-Type", "application/json")
-
     if (username === undefined || username.length == 0) {
         res.status(400).json({
         	status: "username is blank"
@@ -27,29 +21,43 @@ exports.index = function(req, res) {
         return;
     }
 
-    var body = {
-        	callbackUrl: callbackUrl
-        };
+    var callbackUrl = 'https://localhost:9001/api/sync?username='
+    					+ username
+    					+ '&lastSyncDate={{lastSyncDate}}'
+    					+ '&streamId={{streamId}}';
 
-    console.log("about to request");
-    request({
-        method: 'POST',
-        uri: "http://localhost:5000/v1/streams",
-        gzip: true,
-        headers: {
-            "Authorization": "app-id-8aae965172e09b182bede2d71c2b7ebe:app-secret-23e3afadea809f6697d19a8f1754e37df72522b310d57107d5ddb10bda821dd6"
-        },
-        json: true,
-        body: body
-    }, function(error, response, body) {
-        // body is the decompressed response body
-        console.log("response");
-        console.log(error);
-        console.log(response.status);
-        console.log(body);
-        res.json({
+    res.setHeader("Content-Type", "application/json")
+
+    var auth = {
+		appId: "app-id-8aae965172e09b182bede2d71c2b7ebe",
+		appSecret: "app-secret-23e3afadea809f6697d19a8f1754e37df72522b310d57107d5ddb10bda821dd6"
+	}
+
+	var config = {
+		server: 'http://localhost:5000',
+		auth: auth,
+		callbackUrl: callbackUrl
+	}
+
+    Lib1selfserver.createStream(config, function(error, stream){
+    	var barchartUrl = stream.visualize()
+    							.objectTags(["music"])
+    							.actionTags(["listen"])
+    							.count()
+    							.barChart()
+    							.url()
+
+    	var jsonUrl = stream.visualize()
+    							.objectTags(["music"])
+    							.actionTags(["listen"])
+    							.count()
+    							.json()
+    							.url()
+
+		res.json({
 	        "status": "done",
-	        "stream": body
-	    });
+	        "barChart": barchartUrl,
+	        "jsonUrl": jsonUrl
+	    });					 	
     });
 };

4244696cfba98f3661ae034bdbeb64994a467ee7
Devika Deshmukh <ddeshmukh@equalexperts.com>
2015-02-10 15:09:00 +0530

    -- Removed the onData and onResponse functions to solve the memory shoot up issue -- Removed the console logs

--- server/api/sync/sync.controller.js
+++ server/api/sync/sync.controller.js
@@ -19,9 +19,6 @@ var config = {};
 config.server= 'http://api-staging.1self.co';
 // Get list of things
 exports.index = function (req, res) {
-    console.log(req.query);
-
-    console.log('lsd: ' + req.query.lastSyncDate);
     var username = req.query.username;
     var lastSyncDate = req.query.lastSyncDate;
     var streamId = req.query.streamid;
@@ -37,16 +34,10 @@ exports.index = function (req, res) {
             lastSyncDate: config.lastSyncDate
         }
     };
-    console.log('stream is: ' + stream);
     stream.send(eventStart, function (error, response) {
         if (error !== undefined) {
             console.log('error:' + error);
         }
-
-        console.log('sent:');
-        console.log(error);
-        console.log(response);
-        console.log(stream);
         getRecentTracks(username, 1, res, stream, lastSyncDate);
     })
 };
@@ -57,46 +48,23 @@ function getRecentTracks(username, pageNum, res, stream, lastSyncDate) {
     url += "&user=" + username;
 
     if (lastSyncDate !== undefined) {
-
         var unixTimeStamp = Date.parse(lastSyncDate) / 1000;
-        console.log(unixTimeStamp);
         url += "&from=" + unixTimeStamp;
     }
-
-    console.log(url);
-
     request({
         method: 'GET',
         uri: url,
         gzip: true
     }, function (error, response, body) {
-        // body is the decompressed response body
-        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'));
-        console.log(stream);
         onGotArtistTrackData(JSON.parse(body), stream, res, lastSyncDate);
-        console.log("Error: ", error);
-        console.log("Response: ", res);
         if (!res.finished) {
             res.send(200);
         }
-    }).on('data', function (data) {
-        // decompressed data as it is received
-        console.log('decoded chunk: ');
     })
-        .on('response', function (response) {
-            // unmodified http.IncomingMessage object
-            response.on('data', function (data) {
-                // compressed data as it is received
-                console.log('received ' + data.length + ' bytes of compressed data')
-            })
-        })
 }
 
 function onGotArtistTrackData(data, stream, res, lastSyncDate) {
     if (data.recenttracks && data.recenttracks.track) {
-        // console.log('artist tracks here');
-        // console.log(data);
-
         if (data.recenttracks.track.length) {
             for (var i = 0; i < data.recenttracks.track.length; i++) {
                 writeTrack(data.recenttracks.track[i], stream);
@@ -105,7 +73,6 @@ function onGotArtistTrackData(data, stream, res, lastSyncDate) {
             writeTrack(data.recenttracks.track, stream);
         }
     }
-
     if (data.recenttracks && data.recenttracks["@attr"]) {
         if (data.recenttracks["@attr"].totalPages > data.recenttracks["@attr"].page) {
             var nextPage = data.recenttracks["@attr"].page;
@@ -116,58 +83,29 @@ function onGotArtistTrackData(data, stream, res, lastSyncDate) {
 }
 
 function writeTrack(track, stream) {
-    console.log("Got a track");
-    console.log(stream);
-
     if (track.date) {
-
         var dt = new Date();
-
         dt.setTime(track.date.uts * 1000);
-
         var url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=0900562e22abd0500f0432147482cfc1&format=json";
-
         if (track.mbid && track.mbid !== "") {
             url += "&mbid=" + track.mbid;
         } else {
             url += "&artist=" + track.artist['#text'];
             url += "&track=" + track.name;
-            // console.log(track);
         }
-
         request({
             method: 'GET',
             uri: url,
             gzip: true
         }, function (error, response, body) {
-            console.log("onGotTrackData called");
-            console.log(body);
-            if (body === undefined) {
-                console.log("error reading track");
-            }
-            else{
+            if (body !== undefined) {
                 onGotTrackData(JSON.parse(body), track, stream);
             }
-        }).on('data', function (data) {
-            // decompressed data as it is received
-            console.log('decoded chunk: ');
         })
-            .on('response', function (response) {
-                // unmodified http.IncomingMessage object
-                response.on('data', function (data) {
-                    // compressed data as it is received
-                    console.log('received ' + data.length + ' bytes of compressed track data')
-                })
-            })
-
-    } else {
-        console.log("no date");
     }
 }
 
 var sendMusicTo1self = function (trackName, trackmbid, trackDuration, trackUrl, artistName, albumName, listenDate, source, stream) {
-    console.log('send to 1s: ');
-    console.log(stream);
     var dt = new Date();
     dt.setTime(listenDate * 1000);
     var musicEvent = {
@@ -184,12 +122,7 @@ var sendMusicTo1self = function (trackName, trackmbid, trackDuration, trackUrl, 
             "source": source
         }
     };
-
-    console.log(stream);
-    console.log(musicEvent);
     stream.send(musicEvent, function (error, response) {
-        console.log('error: ' + error);
-        console.log('response: ' + response)
     });
 };
 
@@ -197,28 +130,19 @@ function onGotTrackData(data, passedThroughTrack, stream) {
     var html = '';
     var track = data.track;
     var listenDate = passedThroughTrack.date.uts;
-
     if (track) {
         var dt = new Date();
         dt.setTime(listenDate * 1000);
-
         var artistName = "unknown";
         var albumTitle = "unknown";
-
         if (track.artist) {
             artistName = track.artist.name;
         }
         if (track.album) {
             albumTitle = track.album.title;
         }
-        console.log('sending music');
-        console.log(stream);
         sendMusicTo1self(track.name, track.mbid, track.duration, track.url, artistName, albumTitle, listenDate, "last.fm", stream);
-
-        //console.log(JSON.stringify(track));
     } else {
-        console.log(data);
-        console.log(passedThroughTrack);
         failedTrackCount++;
     }
 }

b898e10c9c017f9ad22ea52a49ad4a489d32248b
Anil Wadghule <anildigital@gmail.com>
2015-02-20 15:16:43 +0530

    Update set_env.sh with staging details

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,4 @@
 export INTEGRATION_HOST=http://localhost:9001
 export ONESELF_HOST=http://localhost:5000
-export APP_ID="app-id-2a85e6144f5a87313de0df98ee5133ec"
-export APP_SECRET="app-secret-21835607e69019c01a0b293167172d96ba2549f10b3ff20183e336d6287a78e3"
+export APP_ID="app-id-07be2f65eccd23895b45313a0eb8283b"
+export APP_SECRET="app-secret-e77ca68516aaa9af42746ac65e62ffe09e89235d8e9eb97dd7db6be9bb75a0d9"

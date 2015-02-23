028af5fc56d482e98323016578691c32e33b42e4
Devika Deshmukh <ddeshmukh@equalexperts.com>
2015-02-23 11:47:18 +0530

    -- Using the registration token to decide if the user has come from the add integration page

--- server/api/setup/setup.controller.js
+++ server/api/setup/setup.controller.js
@@ -14,24 +14,45 @@ exports.index = function (req, res) {
     return;
   }
 
-  var callbackUrl =  process.env.INTEGRATION_HOST + '/api/sync?username='
+  var callbackUrl = 'http://localhost:9001/api/sync?username='
     + username
     + '&latestSyncField={{latestSyncField}}'
     + '&streamid={{streamid}}';
 
-  //var config = {
-  //  server: 'https://api-staging.1self.co',
-  //  appId: "app-id-8aae965172e09b182bede2d71c2b7ebe",
-  //  appSecret: "app-secret-23e3afadea809f6697d19a8f1754e37df72522b310d57107d5ddb10bda821dd6",
-  //  callbackUrl: callbackUrl
-  //};
-
+  /*	var config = {
+   server: 'https://api-staging.1self.co',
+   appId: "app-id-8aae965172e09b182bede2d71c2b7ebe",
+   appSecret: "app-secret-23e3afadea809f6697d19a8f1754e37df72522b310d57107d5ddb10bda821dd6",
+   callbackUrl: callbackUrl
+   };
+   */
 
   var config = {
-    server: process.env.ONESELF_HOST,
-    appId: process.env.APP_ID,
-    appSecret: process.env.APP_SECRET,
+    server: 'http://localhost:5000',
+    appId: "abc",
+    appSecret: "123",
     callbackUrl: callbackUrl
+  };
+  var validateUser = function (token) {
+    var deferred = q.defer();
+    request({
+      method: 'GET',
+      uri: config.server + '/v1/validate/user',
+      headers: {
+        'Authorization': token
+      },
+      json: true
+    }, function (e, response, body) {
+      if (response.statusCode === 401) {
+        deferred.reject('Token incorrect', null);
+        return;
+      }
+      if (e || response.statusCode === 500) {
+        deferred.reject("Error: ", e);
+      }
+      deferred.resolve(true);
+    });
+    return deferred.promise;
   };
 
   var createStream = function () {
@@ -64,7 +85,6 @@ exports.index = function (req, res) {
     var deferred = q.defer();
     var callbackUrl = config.callbackUrl.replace('{{streamid}}', stream.streamid);
 
-    console.log("Callback URL after sync: ", config.callbackUrl);
     request({
       method: 'POST', uri: callbackUrl, gzip: true, headers: {
         'Authorization': stream.writeToken
@@ -83,13 +103,20 @@ exports.index = function (req, res) {
     return deferred.promise;
   };
 
-  createStream()
-    .then(function (stream) {
-      console.log("Stream: ", stream);
-      sync(stream);
-      res.status(200).json({
-        "redirectUrl":  process.env.ONESELF_HOST +"/dashboard?streamId="
-        + stream.streamid + "&readToken=" + stream.readToken
+  if (token === undefined) {
+    res.status(200).json({"redirectUrl": "http://localhost:5000/signup"})
+  }
+  else {
+    validateUser(token)
+      .then(createStream)
+      .then(function (stream) {
+        sync(stream);
+        res.status(200).json({
+          "redirectUrl": "http://localhost:5000/dashboard?streamId="
+          + stream.streamid + "&readToken=" + stream.readToken
+        });
+      }).catch(function (error) {
+        res.status(200).json({"redirectUrl": "http://localhost:5000/signup"})
       });
-    });
+  }
 };

5f734c8b369e411c1508d062e0299e6180c93fc0
Ed Sykes <ed.sykes@gmail.com>
2015-05-06 12:56:28 +0100

    added logging

--- server/app.js
+++ server/app.js
@@ -15,6 +15,22 @@ var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
 
+var winston = require('winston');
+winston.add(winston.transports.File, { filename: 'lastfm.log', level: 'debug', json: false });
+winston.level = 'info';
+winston.error('Errors will be logged here');
+winston.warn('Warns will be logged here');
+winston.info('Info will be logged here');
+winston.verbose('Verbose will be logged here');
+winston.debug('Debug will be logged here');
+winston.silly('Silly will be logged here');
+
+var attachLogger = function(req, res, next){
+	req.logger = winston;
+	next();
+};
+app.use(attachLogger);
+  
 // Start server
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));

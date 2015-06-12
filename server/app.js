cea181fce8972be80c9dd816bb658db818bb7df1
Ed Sykes <ed.sykes@gmail.com>
2015-06-12 17:39:20 +0100

    moved the ids up out of the properties

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+var path = require('path');
 var express = require('express');
 var config = require('./config/environment');
 // Setup server
@@ -15,9 +16,12 @@ var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
 
+var logfileLocation = process.env.LOGFILE_LOCATION || './';
+var loggingfilename = path.join(logfileLocation, 'lastfm.log');
 var winston = require('winston');
+
+winston.level = 'debug';
 winston.add(winston.transports.File, { filename: 'lastfm.log', level: 'debug', json: false });
-winston.level = 'info';
 winston.error('Errors will be logged here');
 winston.warn('Warns will be logged here');
 winston.info('Info will be logged here');
@@ -25,15 +29,13 @@ winston.verbose('Verbose will be logged here');
 winston.debug('Debug will be logged here');
 winston.silly('Silly will be logged here');
 
-var attachLogger = function(req, res, next){
-	req.logger = winston;
-	next();
-};
-app.use(attachLogger);
+app.logger = winston;
+
+app.logger.info('PORT=' + process.env.PORT);
   
 // Start server
 server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  app.logger.log('info','Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
 // Expose app
